2b5da48eba805fffa321181c8a2e0ca2082f29fc
MHNavid <hasan.nvd@gmail.com>
2019-09-12 12:45:39 +0600

    main activity and manifest updated

--- app/src/main/java/com/example/whouserapplication/MainActivity.java
+++ app/src/main/java/com/example/whouserapplication/MainActivity.java
@@ -1,14 +1,118 @@
 package com.example.whouserapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView frontPageImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frontPageImageView = findViewById(R.id.FrontPageImageView);
+
+        if (haveNetworkConnection(this)){
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    // this code will be executed after 2 seconds
+//                    Intent intent = new Intent(MainActivity.this, NameEntryActivity.class);
+//                    startActivity(intent);
+                }
+            }, 2000);
+        }else {
+            internetAlert(this);
+        }
+
+        frontPageImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, NameEntryActivity.class);
+//                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean haveNetworkConnection(Context context)
+    {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo)
+        {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+            {
+                if (ni.isConnected())
+                {
+                    haveConnectedWifi = true;
+                    Log.v("WIFI CONNECTION ", "AVAILABLE");
+                } else
+                {
+                    Log.v("WIFI CONNECTION ", "NOT AVAILABLE");
+                }
+            }
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+            {
+                if (ni.isConnected())
+                {
+                    haveConnectedMobile = true;
+                    Log.v("MOBILE CONNECTION ", "AVAILABLE");
+                } else
+                {
+                    Log.v("MOBILE CONNECTION ", "NOT AVAILABLE");
+                }
+            }
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+
+    private void internetAlert(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder
+                .setMessage("Internet not connected.")
+                .setCancelable(false)
+                .setPositiveButton("Check", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (haveNetworkConnection(MainActivity.this)){
+                            new Timer().schedule(new TimerTask() {
+                                @Override
+                                public void run() {
+                                    // this code will be executed after 2 seconds
+//                                    Intent intent = new Intent(MainActivity.this, NameEntryActivity.class);
+//                                    startActivity(intent);
+                                }
+                            }, 2000);
+                        }
+                        else {
+                            internetAlert(MainActivity.this);
+                        }
+
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+        Button pbutton = alert.getButton(DialogInterface.BUTTON_POSITIVE);
+        pbutton.setBackgroundColor(Color.parseColor("#1fab89"));
     }
 }
